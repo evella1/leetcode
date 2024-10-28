4a27da68938f9110e1c86d6918f8c58711ee692d
evella1 <103861598+evella1@users.noreply.github.com>
2024-10-28 13:02:32 -0700

    Create 0146-lru-cache.py

---
+++ python/linked-lists/0146-lru-cache.py
@@ -0,0 +1,71 @@
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+# Implement the LRUCache class:
+# LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+# int get(int key) Return the value of the key if the key exists, otherwise return -1.
+# void put(int key, int value) Update the value of the key if the key exists.
+# Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+# The functions get and put must each run in O(1) average time complexity.
+
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dic = {}
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+
+        node = self.dic[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            old_node = self.dic[key]
+            self.remove(old_node)
+
+        node = ListNode(key, value)
+        self.dic[key] = node
+        self.add(node)
+
+        if len(self.dic) > self.capacity:
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            del self.dic[node_to_delete.key]
+
+    def add(self, node):
+        previous_end = self.tail.prev
+        previous_end.next = node
+        node.prev = previous_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+lRUCache = LRUCache(2)
+lRUCache.put(1, 1) # cache is {1=1}
+lRUCache.put(2, 2) # cache is {1=1, 2=2}
+print(lRUCache.get(1))   # return 1
+lRUCache.put(3, 3) # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+print(lRUCache.get(2))  # returns -1 (not found)
+lRUCache.put(4, 4) # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+print(lRUCache.get(1))    # return -1 (not found)
+print(lRUCache.get(3))    # return 3
+print(lRUCache.get(4))    # return 4
+
+
