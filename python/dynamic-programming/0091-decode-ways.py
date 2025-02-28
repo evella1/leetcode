a6356e3838e06cc3791188bdd6e53f8b6c9d3ec3
evella1 <103861598+evella1@users.noreply.github.com>
2025-02-27 21:43:24 -0800

    Create 0091-decode-ways.py

---
+++ python/dynamic-programming/0091-decode-ways.py
@@ -0,0 +1,58 @@
+
+# 91. Decode Ways
+
+# You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+
+# "1" -> 'A'
+
+# "2" -> 'B'
+
+# ...
+
+# "25" -> 'Y'
+
+# "26" -> 'Z'
+
+# However, while decoding the message, you realize that there are many different ways you can decode the message because some codes are contained in other codes ("2" and "5" vs "25").
+
+# For example, "11106" can be decoded into:
+
+# "AAJF" with the grouping (1, 1, 10, 6)
+# "KJF" with the grouping (11, 10, 6)
+# The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+# Note: there may be strings that are impossible to decode.
+
+# Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+
+# The test cases are generated so that the answer fits in a 32-bit integer.
+
+def numDecodings(s: str) -> int:
+    if not s or s[0] == "0":
+        return 0
+
+    n = len(s)
+    dp = [0] * (n + 1)
+    dp[n] = 1
+
+    for i in range(n - 1, -1, -1):
+        if s[i] == "0":
+            dp[i] = 0
+        else:
+            dp[i] = dp[i + 1]
+
+        if i + 1 < n and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+            dp[i] += dp[i + 2]
+
+    return dp[0]
+
+s1 = "12"
+s2 = "226"
+s3 = "0"
+s4 = "06"
+s5 = "1232126"
+
+print(numDecodings(s1)) # 2
+print(numDecodings(s2)) # 3
+print(numDecodings(s3)) # 0
+print(numDecodings(s4)) # 0
+print(numDecodings(s5)) # 15
